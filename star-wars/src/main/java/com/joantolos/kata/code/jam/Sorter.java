a46a5f388883b5d511e0888d5ae327414aeececd
joan.tolos <joan.tolos@clarivate.com>
2018-04-26 16:08:17 +0200

    Minor naming refactoring

--- star-wars/src/main/java/com/joantolos/kata/code/jam/Sorter.java
+++ star-wars/src/main/java/com/joantolos/kata/code/jam/Sorter.java
@@ -8,7 +8,7 @@ public class Sorter {
 
     public String[] sort(String[] target, Integer[] order) {
         List<String> orderedTarget = new ArrayList<>();
-        Arrays.stream(order).forEach(o -> orderedTarget.add(target[o - 1]));
+        Arrays.stream(order).forEach(moviePosition -> orderedTarget.add(target[moviePosition - 1]));
         return orderedTarget.toArray(new String[target.length]);
     }
 }
