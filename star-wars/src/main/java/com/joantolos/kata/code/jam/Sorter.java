91ee96a7da10b3138f4d57efe182a7a3564175aa
joan.tolos <joan.tolos@clarivate.com>
2018-04-26 16:05:11 +0200

    First implementation

--- star-wars/src/main/java/com/joantolos/kata/code/jam/Sorter.java
+++ star-wars/src/main/java/com/joantolos/kata/code/jam/Sorter.java
@@ -1,8 +1,14 @@
 package com.joantolos.kata.code.jam;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Sorter {
 
     public String[] sort(String[] target, Integer[] order) {
-        return new String[] {"The Phantom Menace (1999)", "Attack of the Clones (2002)", "Revenge of the Sith (2005)", "Solo: A Star Wars Story (2018)", "Rogue One: A Star Wars Story (2016)", "A New Hope (1977)", "The Empire Strikes Back (1980)", "Return of the Jedi (1983)", "The Force Awakens (2015)", "The Last Jedi (2017)"};
+        List<String> orderedTarget = new ArrayList<>();
+        Arrays.stream(order).forEach(o -> orderedTarget.add(target[o - 1]));
+        return orderedTarget.toArray(new String[target.length]);
     }
 }
