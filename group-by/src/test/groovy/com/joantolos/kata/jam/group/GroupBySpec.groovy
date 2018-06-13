781e2fc32bdc0126e5302df0afb7b99553abc3ed
joan.tolos <joan.tolos@clarivate.com>
2018-06-13 13:20:18 +0200

    Test refactors

--- group-by/src/test/groovy/com/joantolos/kata/jam/group/GroupBySpec.groovy
+++ group-by/src/test/groovy/com/joantolos/kata/jam/group/GroupBySpec.groovy
@@ -5,14 +5,12 @@ import spock.lang.Specification
 
 class GroupBySpec extends Specification {
 
+    @Shared Map<String, Integer> scores
     @Shared GroupBy groupBy
+
     def setupSpec() {
         groupBy = new GroupBy()
-    }
-
-    def 'Should group by scores'() {
-
-        Map<String, Integer> scores = new HashMap<>()
+        scores = new HashMap<>()
 
         scores.put("Jack", 12)
         scores.put("Jill", 15)
@@ -20,10 +18,20 @@ class GroupBySpec extends Specification {
         scores.put("Darla", 15)
         scores.put("Nick", 15)
         scores.put("Nancy", 11)
+    }
 
+    def 'Tom and Nancy must have 11 score'() {
         expect:
         groupBy.scores(scores).get(11).containsAll(["Tom", "Nancy"])
+    }
+
+    def 'Jack must have 12 score'() {
+        expect:
         groupBy.scores(scores).get(12).containsAll(["Jack"])
+    }
+
+    def 'Jill, Darla and Nick must have 15 score'() {
+        expect:
         groupBy.scores(scores).get(15).containsAll(["Jill", "Darla", "Nick"])
     }
 }
