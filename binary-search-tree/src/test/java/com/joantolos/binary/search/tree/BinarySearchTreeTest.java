09e7a781daf4f90faf9168feb697ccfa317d5d2d
joantolos <joan.tolos@gmail.com>
2022-08-11 15:03:46 +0200

    Binary search tree tests in place

--- binary-search-tree/src/test/java/com/joantolos/binary/search/tree/BinarySearchTreeTest.java
+++ binary-search-tree/src/test/java/com/joantolos/binary/search/tree/BinarySearchTreeTest.java
@@ -1,4 +1,21 @@
 package com.joantolos.binary.search.tree;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BinarySearchTreeTest {
+
+    private final BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+    @Test
+    public void shouldComplyWithExampleOne() {
+        BinarySearchTree.TreeNode treeOne = new BinarySearchTree.TreeNode(2, new BinarySearchTree.TreeNode(1), new BinarySearchTree.TreeNode(3));
+        Assert.assertTrue(binarySearchTree.isValidBST(treeOne));
+    }
+
+    @Test
+    public void shouldComplyWithExampleTwo() {
+        BinarySearchTree.TreeNode treeTwo = new BinarySearchTree.TreeNode(5, new BinarySearchTree.TreeNode(1), new BinarySearchTree.TreeNode(4, new BinarySearchTree.TreeNode(3), new BinarySearchTree.TreeNode(6)));
+        Assert.assertFalse(binarySearchTree.isValidBST(treeTwo));
+    }
 }
